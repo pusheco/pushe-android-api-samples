a39f0ae40baf49b4eb0ae43342b4ee6bc012f42c
Ali Vakilzade <ali@vakilzade.com>
2018-08-19 14:06:14 +0430

    simple topic added

---
+++ php/simple_topic.php
@@ -0,0 +1,29 @@
+<?php
+
+$TOKEN = "YOUR_TOKEN";
+
+$data = array(
+  "applications" => ["YOUR_APPLICATION_ID", ],
+  "notification" => array(
+    "title" => "this is the title",
+    "content" => "this is the content",
+  ),
+  "topics" => array(
+    "Topic1",
+    "Topic2",
+  ),
+);
+$data_string = json_encode($data);
+
+$ch = curl_init("http://panel.pushe.co/api/v1/notifications/");
+curl_setopt( $ch, CURLOPT_HTTPHEADER, array(
+  "Content-Type: application/json",
+  "Accept: application/json",
+  "Authorization: Token " . $TOKEN,
+));
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+$output = curl_exec($ch);
+echo $output
+
+?>
