79e2683e6f71841b0cc23546bdd560dc4107d4d4
Ali Vakilzade <ali@vakilzade.com>
2018-11-26 15:45:01 +0330

    also print report_url for simple push

--- php/simple_push.php
+++ php/simple_push.php
@@ -9,17 +9,17 @@ $data = array(
         "content" => "this is the content",
     ),
 );
-$data_string = json_encode($data);
 
 $ch = curl_init("https://api.pushe.co/v2/messaging/notifications/");
+
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     "Content-Type: application/json",
     "Accept: application/json",
     "Authorization: Token " . $TOKEN,
 ));
 
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $reponse = curl_exec($ch);
@@ -27,6 +27,26 @@ $status_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 curl_close($ch);
 
 echo "status code => $status_code\n";
-echo "response => $reponse"
+echo "response => $reponse\n";
+echo "==========\n";
+
+if ($status_code == 201) {
+    echo "Success!\n";
+
+    // notification created
+    $reponse_json = json_decode($reponse);
+
+    if ($reponse_json->hashed_id) {
+        $report_url = "https://pushe.co/report?id=$reponse_json->hashed_id";
+    } else {
+        $report_url = "no report url for free plan";
+    }
+    echo "report_url: $report_url\n";
+
+    echo "notification id: $reponse_json->wrapper_id\n";
+} else {
+    // creating notification failed
+    echo "failed!\n";
+}
 
 ?>
