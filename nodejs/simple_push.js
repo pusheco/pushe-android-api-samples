1264201513e6e099172cba0381bab283324f48d6
Ali Vakilzade <ali@vakilzade.com>
2018-12-08 11:58:13 +0330

    use commonjs for sample code

--- nodejs/simple_push.js
+++ nodejs/simple_push.js
@@ -1,6 +1,6 @@
 var request = require('request');
 
-let TOKEN = "YOUR_TOKEN";
+var TOKEN = "YOUR_TOKEN";
 
 request.post(
     {
@@ -19,25 +19,25 @@ request.post(
             }
         }),
     },
-    (error, response, body) => {
-        console.log(`status_code: ${response.statusCode}`)
-        console.log(`response: ${body}`);
+    function(error, response, body) {
+        console.log("status_code: " + response.statusCode);
+        console.log("response: " + body);
 
         if (response.statusCode == 201){
             console.log("Success!");
 
             var data = JSON.parse(body);
-            let report_url;
+            var report_url;
 
             if (data.hashed_id) {
-                report_url = `https://pushe.co/report?id=${data.hashed_id}`;
+                report_url = "https://pushe.co/report?id=" + data.hashed_id;
             } else {
                 report_url = "no report url for your plan";
             }
 
-            console.log(`report_url: ${report_url}`);
+            console.log("report_url: " + report_url);
 
-            console.log(`notification id: ${data.wrapper_id}`);
+            console.log("notification id: " + data.wrapper_id);
         } else {
             console.log("failed!");
         }
